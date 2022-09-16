20ac6931c030f100d7473fa9d1df9f911a188fe2
Luan <luanrodriguesdev@gmail.com>
2022-09-16 10:23:50 -0300

    finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,63 @@
-import './styles/main.css'
-import logoImg from './assets/logo.svg'
-import * as Dialog from '@radix-ui/react-dialog';
+import "./styles/main.css";
+import logoImg from "./assets/logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-import { GameBanner } from './components/GameBanner'
-import { CreateAdBanner } from './components/CreateAdBanner'
-import { CreateAdModal } from './components/CreateAdModal';
-
+import { GameBanner } from "./components/GameBanner";
+import { CreateAdBanner } from "./components/CreateAdBanner";
+import { CreateAdModal } from "./components/CreateAdModal";
+import axios from "axios";
 
 interface Game {
-  id: string,
-  title: string,
-  banner: string,
+  id: string;
+  title: string;
+  banner: string;
   _count: {
-    ads: number
-  }
+    ads: number;
+  };
 }
 
 function App() {
-  const [games, setGames] = useState<Game[]>([])
+  const [games, setGames] = useState<Game[]>([]);
 
   useEffect(() => {
-      fetch('http://localhost:3333/games')
-      .then((response) => {
-        response.json().then((data) =>{
-          setGames(data)
-        })
-      }) 
-  }, [])
+    axios("http://localhost:3333/games").then((response) => {
+      setGames(response.data);
+    });
+  }, []);
 
   return (
-    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} />
 
-      <h1 className="text-6xl text-white font-black mt-20">Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui!</h1>
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          duo
+        </span>{" "}
+        está aqui!
+      </h1>
 
-      <div className='grid grid-cols-6 gap-6 mt-16'>
+      <div className="grid grid-cols-6 gap-6 mt-16">
         {games.map((game) => {
           return (
             <GameBanner
               key={game.id}
-              banner={game.banner} 
-              title={game.title} 
+              banner={game.banner}
+              title={game.title}
               adsCount={game._count.ads}
-            /> 
-          )
+            />
+          );
         })}
-        
       </div>
-      
+
       <Dialog.Root>
         <CreateAdBanner />
         <CreateAdModal />
       </Dialog.Root>
-           
     </div>
-  ) 
+  );
 }
 
-export default App
+export default App;
